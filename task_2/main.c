dac06a1a37b424d02522729696d2b6a6b0bb5935
bbshkkrll <bobeshko@icloud.com>
2024-04-17 17:12:36 +0500

    sigterm handler added

--- task_2/main.c
+++ task_2/main.c
@@ -4,43 +4,63 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+
+int success_attempt_count = 0;
+int attempt_count = 0;
+
+
+void termination_handler(int signum) {
+    FILE* f_result = fopen("result.txt", "w");
+    fprintf(f_result, "[%d] %d/%d (success/all)\n", getpid(), success_attempt_count, attempt_count);
+    fclose(f_result);
+
+    exit(0);
+}
+
 
 int main(int argc, char *argv[])
 {
-    printf("My pid:%d\n", getpid());
 
-    char *source_filename = argv[1];
-    char lockfile[128];
+    signal(SIGINT, termination_handler);
+    while (1) {
+        char *source_filename = argv[1];
+        char lockfile[128];
+        sleep(0.0003);
+        sprintf(lockfile, "%s.lck", source_filename);
 
-    sprintf(lockfile, "%s.lck", source_filename);
-
-    printf("Source filename is %s\n", source_filename);
-    printf("Lock filename is %s\n", lockfile);
-
-    int fd = open(lockfile, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
-
-    if (fd < 0) {
-        if (errno == EEXIST) {
-            printf("Lockfile file exits\n");
-
-            while (access(lockfile, F_OK) == 0) {
-                printf("Pending for deletion\n");
-                sleep(1);
-            }
+        while (access(lockfile, F_OK) == 0) {
+             sleep(0.0001);
         }
 
-    } else {
-        printf("Lock file doesn't exist\n");
+        printf("[%d] Creating lockfile\n", getpid());
+        int lock_fd = open(lockfile, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
+        char spid[16];
+        sprintf(spid, "%d", getpid());
+        write(lock_fd, spid, strlen(spid));
+        close(lock_fd);
+
+        int source_fd = open(source_filename, O_WRONLY, S_IRUSR | S_IWUSR);
+        lseek(source_fd, 0, SEEK_END);
+        write(source_fd, ".", 1);
+        sleep(3);
+        close(source_fd);
+
+       int stored_pid;
+       char input[8];
+       lock_fd = open(lockfile, O_RDONLY, S_IRUSR | S_IWUSR);
+       read(lock_fd, input, sizeof(input));
+
+       stored_pid = atoi(input);
+
+       if (stored_pid == getpid()) {
+           printf("[%d] Removing %s\n", getpid(), lockfile);
+           remove(lockfile);
+       } else {
+           exit(1);
+           printf("[%d] Exit...\n", getpid());
+       }
     }
-
-    printf("Creating .lck");
-    fd = open(lockfile, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
-
-    char spid[16];
-    sprintf(spid, "%d", getpid());
-    write(fd, spid, strlen(spid));
-
-    sleep(3);
 
     return 0;
 }
